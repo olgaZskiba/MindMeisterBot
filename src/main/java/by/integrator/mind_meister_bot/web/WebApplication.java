04e0e15462b95ce4ae293af02518b6e9facad5b6
avp02 <vpetranovskij@gmail.com>
2021-07-27 00:56:14 +0300

    added maven

---
+++ src/main/java/by/integrator/mind_meister_bot/web/WebApplication.java
@@ -0,0 +1,11 @@
+package by.integrator.mind_meister_bot.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
