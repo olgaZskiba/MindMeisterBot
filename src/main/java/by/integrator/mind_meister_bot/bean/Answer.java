41b6a72d2d336b548d3cb4eb7f1592cb4b12f9e1
zhdanok <zhanokla@gmail.com>
2021-07-27 13:31:13 +0300

    added beans and repositiries

---
+++ src/main/java/by/integrator/mind_meister_bot/bean/Answer.java
@@ -0,0 +1,38 @@
+package by.integrator.mind_meister_bot.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "answer")
+public class Answer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "answer_id")
+    private Long answerId;
+
+    @Column(name = "text_answer")
+    private String textAnswer;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "profile_id", nullable = false)
+    private Profile profile;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+
+
+
+
+}
---
+++ src/main/java/by/integrator/mind_meister_bot/bean/Question.java
@@ -0,0 +1,29 @@
+package by.integrator.mind_meister_bot.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "question")
+public class Question implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "question_id")
+    private Long questionId;
+
+    @Column(name = "text_question")
+    private String textQuestion;
+
+
+
+
+}
