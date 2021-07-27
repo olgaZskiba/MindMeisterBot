1a68c1ed04519a3ba926b147fe8f930461031d7f
olgaz <olgaz.sweetheart@gmail.com>
2021-07-27 18:21:22 +0300

    adds security files

--- src/main/java/by/integrator/mind_meister_bot/bot/MindMeisterBot.java
+++ src/main/java/by/integrator/mind_meister_bot/bot/MindMeisterBot.java
@@ -1,23 +1,23 @@
-package by.integrator.mind_meister_bot.bot;
-
-import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-@Component
-public class MindMeisterBot extends TelegramLongPollingBot {
-    @Override
-    public void onUpdateReceived(Update update) {
-
-    }
-
-    @Override
-    public String getBotUsername() {
-        return null;
-    }
-
-    @Override
-    public String getBotToken() {
-        return null;
-    }
-}
+//package by.integrator.mind_meister_bot.bot;
+//
+//import org.springframework.stereotype.Component;
+//import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+//import org.telegram.telegrambots.meta.api.objects.Update;
+//
+//@Component
+//public class MindMeisterBot extends TelegramLongPollingBot {
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//
+//    }
+//
+//    @Override
+//    public String getBotUsername() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getBotToken() {
+//        return null;
+//    }
+//}
--- src/main/java/by/integrator/mind_meister_bot/bot/bean/Profile.java
+++ src/main/java/by/integrator/mind_meister_bot/bot/bean/Profile.java
@@ -1,15 +1,14 @@
 package by.integrator.mind_meister_bot.bot.bean;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,8 +18,8 @@ public class Profile implements Serializable {
     public Profile(Long chatId, String userName, Integer amount, Double percentage) {
         this.chatId = chatId;
         this.userName = userName;
-        this.amount = amount;
-        this.percentage = percentage;
+//        this.amount = amount;
+//        this.percentage = percentage;
     }
 
     @Id
@@ -28,15 +27,26 @@ public class Profile implements Serializable {
     @Column(name = "profile_id")
     private Long profileId;
 
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "profile_role", joinColumns = @JoinColumn(name = "id_profile"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
     @Column(name = "chat_id")
     private Long chatId;
 
     @Column(name = "username")
     private String userName;
 
-    @Column(name = "amount", columnDefinition = "0")
-    private Integer amount;
-
-    @Column(name = "percentage", columnDefinition = "0.0")
-    private Double percentage;
+//    @Column(name = "amount", columnDefinition = "0")
+//    private Integer amount;
+//
+//    @Column(name = "percentage", columnDefinition = "0.0")
+//    private Double percentage;
 }
